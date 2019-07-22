ea707c18b4ef5c2893b0dec514d10ace70723b47
Lettucech <pingkiuho@gmail.com>
2019-07-22 16:01:19 +0800

    added state list animator

--- app/src/main/java/io/github/lettucech/example/android/viewanimations/fragment/PropertyAnimationFragment.kt
+++ app/src/main/java/io/github/lettucech/example/android/viewanimations/fragment/PropertyAnimationFragment.kt
@@ -53,7 +53,12 @@ class PropertyAnimationFragment : Fragment() {
             }
 
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
-                spinner_interpolator.isEnabled = spinner_animator.getItemAtPosition(position) != AnimatorType.ANIMATOR_SET.label
+                when (spinner_animator.getItemAtPosition(position)) {
+                    AnimatorType.ANIMATOR_SET.label,
+                    AnimatorType.ANIMATE_LAYOUT_CHANGES.label,
+                    AnimatorType.STATE_LIST_ANIMATOR.label -> spinner_interpolator.isEnabled = false
+                    else -> spinner_interpolator.isEnabled = true
+                }
             }
 
         }
@@ -71,21 +76,42 @@ class PropertyAnimationFragment : Fragment() {
 
 
         btn_start_animation.setOnClickListener {
+            btn_start_animation.isEnabled = false
             when (spinner_animator.selectedItem) {
                 AnimatorType.VALUE_ANIMATOR.label -> {
-                    logViewModel?.addLog("Triggered ValueAnimation")
+                    logViewModel?.addLog("Triggered Value Animation")
                     startValueAnimation()
                 }
                 AnimatorType.OBJECT_ANIMATOR.label -> {
-                    logViewModel?.addLog("Triggered ObjectAnimation")
+                    logViewModel?.addLog("Triggered Object Animation")
                     startObjectAnimation()
                 }
                 AnimatorType.ANIMATOR_SET.label -> {
-                    logViewModel?.addLog("Triggered AnimatorSet")
+                    logViewModel?.addLog("Triggered Animator Set")
                     startAnimatorSet()
+                }
+                AnimatorType.ANIMATE_LAYOUT_CHANGES.label -> {
+                    logViewModel?.addLog("Triggered Animate Layout Changes")
+                    startAnimateLayoutChanges()
+                }
+                AnimatorType.STATE_LIST_ANIMATOR.label -> {
+                    logViewModel?.addLog("Triggered State List Animator")
+                    startStateListAnimator()
                 }
             }
         }
+    }
+
+    private fun resetAnimationObject() {
+        view_animation_object.translationX = 0f
+        view_animation_object.scaleX = 1f
+        view_animation_object.scaleY = 1f
+        view_animation_object.setBackgroundColor(
+            ContextCompat.getColor(
+                requireContext(),
+                R.color.colorPrimary
+            )
+        )
     }
 
     private fun getSelectedInterpolator(): TimeInterpolator? {
@@ -111,10 +137,17 @@ class PropertyAnimationFragment : Fragment() {
                 logViewModel?.addLog("Change view translationX to " + updatedAnimation.animatedValue)
                 view_animation_object.translationX = updatedAnimation.animatedValue as Float
             }
+            addListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator?) {
+                    super.onAnimationEnd(animation)
+                    btn_start_animation.isEnabled = true
+                }
+            })
             val interpolatorClassName = interpolator::class.java.simpleName
             logViewModel?.addLog("Set:\nDuration = $duration\nInterpolator = $interpolatorClassName")
         }
 
+        resetAnimationObject()
         animator.start()
         logViewModel?.addLog("Start animation")
     }
@@ -135,11 +168,12 @@ class PropertyAnimationFragment : Fragment() {
         animator.addListener(object : AnimatorListenerAdapter() {
             override fun onAnimationEnd(animation: Animator?) {
                 logViewModel?.addLog("Animation finished object translationX is now " + view_animation_object.translationX)
+                btn_start_animation.isEnabled = true
             }
         })
 
         logViewModel?.addLog("Reset animation object translationX to 0f")
-        view_animation_object.translationX = 0f
+        resetAnimationObject()
 
         animator.start()
         logViewModel?.addLog("Start animation")
@@ -208,21 +242,74 @@ class PropertyAnimationFragment : Fragment() {
             addListener(object : AnimatorListenerAdapter() {
                 override fun onAnimationEnd(animation: Animator?) {
                     logViewModel?.addLog("All animator played")
+                    btn_start_animation.isEnabled = true
                 }
             })
         }
 
         logViewModel?.addLog("Reset animation object translationX to 0f")
-        view_animation_object.translationX = 0f
-        view_animation_object.scaleX = 1f
-        view_animation_object.scaleY = 1f
-        view_animation_object.setBackgroundColor(
-            ContextCompat.getColor(
-                requireContext(),
-                R.color.colorPrimary
-            )
-        )
+        resetAnimationObject()
         animatorSet.start()
+    }
+
+    private fun startAnimateLayoutChanges() {
+        logViewModel?.addLog("Create LayoutTransition and assign to root layout")
+        layout_root.layoutTransition = LayoutTransition().apply {
+            setDuration(1000)
+            addTransitionListener(object : LayoutTransition.TransitionListener {
+                var goneAnimation = true
+
+                override fun startTransition(
+                    p0: LayoutTransition?,
+                    p1: ViewGroup?,
+                    p2: View?,
+                    p3: Int
+                ) {
+                    logViewModel?.addLog("Create LayoutTransition and assign to root layout")
+                }
+
+                override fun endTransition(
+                    p0: LayoutTransition?,
+                    p1: ViewGroup?,
+                    p2: View?,
+                    p3: Int
+                ) {
+                    if (!goneAnimation) {
+                        logViewModel?.addLog("Remove LayoutTransition from to root layout")
+                        layout_root.layoutTransition = null
+                        btn_start_animation.isEnabled = true
+                    } else {
+                        goneAnimation = false
+                    }
+                }
+
+            })
+        }
+        resetAnimationObject()
+
+        logViewModel?.addLog("Set animation object visibility to GONE")
+        view_animation_object.visibility = View.GONE
+
+
+        logViewModel?.addLog("Set animation object visibility to VISIBLE")
+        view_animation_object.visibility = View.VISIBLE
+    }
+
+    private fun startStateListAnimator() {
+        resetAnimationObject()
+
+        view_animation_object.stateListAnimator =
+            AnimatorInflater.loadStateListAnimator(requireContext(), R.xml.animate_scale)
+
+        view_animation_object.setOnClickListener {
+            it.setOnClickListener(null)
+            textView_instruction.visibility = View.GONE
+            textView_instruction.text = null
+            btn_start_animation.isEnabled = true
+        }
+
+        textView_instruction.visibility = View.VISIBLE
+        textView_instruction.text = "Press the square"
     }
 }
 
--- app/src/main/java/io/github/lettucech/example/android/viewanimations/util/AnimatorType.kt
+++ app/src/main/java/io/github/lettucech/example/android/viewanimations/util/AnimatorType.kt
@@ -4,9 +4,11 @@ package io.github.lettucech.example.android.viewanimations.util
  * Created by Brian Ho on 2019-07-16.
  */
 enum class AnimatorType(val label: String) {
-    VALUE_ANIMATOR("ValueAnimator"),
-    OBJECT_ANIMATOR("ObjectAnimator"),
-    ANIMATOR_SET("AnimatorSet");
+    VALUE_ANIMATOR("Value Animator"),
+    OBJECT_ANIMATOR("Object Animator"),
+    ANIMATOR_SET("Animator Set"),
+    ANIMATE_LAYOUT_CHANGES("Animate Layout Changes"),
+    STATE_LIST_ANIMATOR("State List Animator");
 
     companion object {
         fun toLabelArray() = ArrayList<String>().apply {
